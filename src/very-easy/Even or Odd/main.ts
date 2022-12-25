0b5fb88d03eb2ca515c4326fddbd029b58c3bc6f
Charlie Martin <charliejm7@gmail.com>
2022-12-25 16:50:04 +0000

    refactor

--- src/very-easy/Even or Odd/main.ts
+++ src/very-easy/Even or Odd/main.ts
@@ -4,5 +4,4 @@ https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
 Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 */
 
-export const evenOrOdd = (number: number) =>
-  number % 2 === 1 || number % 2 === -1 ? 'Odd' : 'Even';
+export const evenOrOdd = (number: number) => (number % 2 ? 'Odd' : 'Even');
